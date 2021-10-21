31b8ec5b252a04cb0d9f7e8fb480e2550cb92e92
Raine McDevitt <raine.mcdevitt@gmail.com>
2021-10-20 19:37:10 -0500

    added viewmodel

---
+++ TechJobsPersistent/ViewModels/AddEmployerViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TechJobsPersistent.ViewModels
+{
+    public class AddEmployerViewModel
+    {
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Location is required")]
+        public string Location { get; set; }
+
+        //NOT SURE IF NEEDED:
+        public AddEmployerViewModel(string name, string location)
+        {
+            Name = name;
+            Location = location;
+        }
+
+        public AddEmployerViewModel()
+        {
+        }
+    }
+}
