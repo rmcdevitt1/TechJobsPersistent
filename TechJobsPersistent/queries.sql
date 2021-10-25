0f1f26588a693e75395a8017412d1f498a6dd03b
Raine McDevitt <raine.mcdevitt@gmail.com>
2021-10-24 21:46:32 -0500

    pt2 done except error

--- TechJobsPersistent/queries.sql
+++ TechJobsPersistent/queries.sql
@@ -1,9 +1,15 @@
---Part 1: List the columns and their data types in the Jobs table.
---id INT
---name: VARCHAR(200)
---employerId: INT
+/*
+Part 1: List the columns and their data types in the Jobs table.
 
---Part 2:
+id INT
+name VARCHAR(200)
+employerId INT
 
---Part 3:
+Part 2: List the names of the employers in St. Louis City.
+SELECT Name FROM employers
+WHERE Location = "STL";
 
+Part 3:
+
+
+*/
